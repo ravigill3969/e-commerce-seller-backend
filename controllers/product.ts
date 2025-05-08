ee3ed6e2bfaea9755b29165ca5f89691a5db8a93
ravigill3936 <gill1234ravi@gmail.com>
2025-05-08 18:52:04 -0400

    product storing functionlity is working nice and precise

--- controllers/product.ts
+++ controllers/product.ts
@@ -1,15 +1,22 @@
 import { Request, Response, NextFunction } from "express";
 import { catchAsync } from "../utils/asyncHandler";
 import { uploadToCloudinary } from "../utils/cloudinary";
+import Product from "../models/product";
+import { AppError } from "../utils/AppError";
 
 export const addProduct = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const { productName, price, stockQuantity, category, brand, desccription } =
+    const { productName, price, stockQuantity, category, brand, description } =
       req.body;
 
     const files = req.files as
       | { [fieldname: string]: Express.Multer.File[] }
       | Express.Multer.File[];
+
+    if (!files) {
+      next(new AppError("Images are required!", 403));
+      return;
+    }
 
     const filesArray: Express.Multer.File[] = Array.isArray(files)
       ? files
@@ -17,6 +24,25 @@ export const addProduct = catchAsync(
 
     const urls = await uploadToCloudinary(filesArray);
 
-    console.log(urls)
+    const product = await Product.create({
+      productName,
+      stockQuantity,
+      price,
+      category,
+      brand,
+      description,
+      photoURLs: urls,
+      sellerID: req.userId,
+    });
+
+    if (!product) {
+      next(new AppError("Unable to create product, try again later!", 500));
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Product created successfully!",
+    });
   }
 );
--- models/product.ts
+++ models/product.ts
@@ -1,5 +1,33 @@
-import mongoose from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 
-interface IProduct extends Document {
-    productName : string
+export interface IProduct extends Document {
+  productName: string;
+  price: number;
+  stockQuantity: number;
+  category: string;
+  brand: string;
+  description: string;
+  photoURLs: string[];
+  sellerID: mongoose.Types.ObjectId;
+  isActive: boolean;
 }
+
+const ProductSchema: Schema = new Schema(
+  {
+    productName: { type: String, required: true },
+    sellerID: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    price: { type: Number, required: true },
+    stockQuantity: { type: Number, required: true },
+    category: { type: String, required: true },
+    brand: { type: String, required: true },
+    description: { type: String, required: true },
+    photoURLs: [{ type: String }],
+    isActive: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model<IProduct>("Product", ProductSchema);
+export default Product;
